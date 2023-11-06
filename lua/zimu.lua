6c285db2aaddc653da372afc05955830e7380949
for <ananke@163.com>
2023-11-06 10:11:35 +0800

    Add files via upload
    修改大量的词汇

--- lua/zimu.lua
+++ lua/zimu.lua
@@ -1,5 +1,43 @@
--- 字母数字转成对应的𝑨𝑩𝑪𝑫𝑬𝑭𝑮𝑯𝑰𝑱𝑲𝑳𝑴𝑵𝑶𝑷𝑸𝑹𝑺𝑻𝑼𝑽𝑾𝑿𝒀𝒁𝟎𝟏𝟐𝟑𝟒𝟓𝟔𝟕𝟖𝟗𝟬𝟭𝟮𝟯𝟰𝟱𝟲𝟳𝟵, 按 a 开启
+-- 字母数字转成对应的𝑨𝑩𝑪𝑫𝑬𝑭𝑮𝑯𝑰𝑱𝑲𝑳𝑴𝑵𝑶𝑷𝑸𝑹𝑺𝑻𝑼𝑽𝑾𝑿𝒀𝒁𝟎𝟏𝟐𝟑𝟒𝟓𝟔𝟕𝟖𝟗𝟬𝟭𝟮𝟯𝟰𝟱𝟲𝟳𝟵
 local alphabet = {
+	a = '𝒂',
+    b = '𝒃',
+    c = '𝒄',
+    d = '𝒅',
+    e = '𝒆',
+    f = '𝒇',
+    g = '𝒈',
+    h = '𝒉',
+    i = '𝒊',
+    j = '𝒋',
+    k = '𝒌',
+    l = '𝒍',
+    m = '𝒎',
+    n = '𝒏',
+    o = '𝒐',
+    p = '𝒑',
+    q = '𝒒',
+    r = '𝒓',
+    s = '𝒔',
+    t = '𝒕',
+    u = '𝒖',
+    v = '𝒗',
+    w = '𝒘',
+    x = '𝒙',
+    y = '𝒚',
+    z = '𝒛',
+    ['0'] = '𝟶',
+    ['1'] = '𝟷',
+    ['2'] = '𝟸',
+    ['3'] = '𝟹',
+    ['4'] = '𝟺',
+    ['5'] = '𝟻',
+    ['6'] = '𝟼',
+    ['7'] = '𝟽',
+    ['8'] = '𝟾',
+    ['9'] = '𝟿'
+}
+local alphabet2 = {
     a = '𝑨',
     b = '𝑩',
     c = '𝑪',
@@ -37,26 +75,63 @@ local alphabet = {
     ['8'] = '𝟴',
     ['9'] = '𝟵'
 }
-
 local function translator(input, seg, env)
-
-    if string.sub(input, 1, 1) == "-" then
-        -- 截取输入的后面部分 
-        local input2 = string.sub(input, 2)
-
+    local trans_table = alphabet2
+    local start_pos = 0
+    if string.sub(input, 1, 2) == "\\\\" then
+        trans_table = alphabet
+        start_pos = 3
+    else
+        if string.sub(input, 1, 1) == "\\" then
+            start_pos = 2
+        end
+    end
+    if start_pos ~= 0 then
+        local input2 = string.sub(input, start_pos)
         -- 逐字母替换
         local output = ""
         for i = 1, string.len(input2) do
             local char = string.sub(input2, i, i)
-            if alphabet[char] then
-                output = output .. alphabet[char]
+            if trans_table[char] then
+                output = output .. trans_table[char]
             else
                 output = output .. char
             end
         end
         return yield(Candidate("text", seg.start, seg._end, output, "转"))
-
     end
+    -- if string.sub(input, 1, 1) == "-" then
+    --     -- 截取输入的后面部分 
+    --     local input2 = string.sub(input, 2)
+
+    --     -- 逐字母替换
+    --     local output = ""
+    --     for i = 1, string.len(input2) do
+    --         local char = string.sub(input2, i, i)
+    --         if alphabet[char] then
+    --             output = output .. alphabet[char]
+    --         else
+    --             output = output .. char
+    --         end
+    --     end
+    --     return yield(Candidate("text", seg.start, seg._end, output, "大"))
+    -- end
+    -- if string.sub(input, 1, 1) == "+" then
+    --     -- 截取输入的后面部分 
+    --     local input2 = string.sub(input, 2)
+
+    --     -- 逐字母替换
+    --     local output = ""
+    --     for i = 1, string.len(input2) do
+    --         local char = string.sub(input2, i, i)
+    --         if alphabet2[char] then
+    --             output = output .. alphabet2[char]
+    --         else
+    --             output = output .. char
+    --         end
+    --     end
+    --     return yield(Candidate("text", seg.start, seg._end, output, "小"))
+    -- end
 
 end
 
